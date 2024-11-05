9bab45346cd05a485b1111edde3bee7a4f9e0aab
PoXel6 <iampoxel6@gmail.com>
2024-11-05 16:10:05 +0000

    fixed (big word) tmux

--- nvim/.config/nvim/lua/config/lsp-config.lua
+++ nvim/.config/nvim/lua/config/lsp-config.lua
@@ -1,99 +1,98 @@
 local lsp_zero = require("lsp-zero")
 -- Setup LSP mappings and configuration
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(event)
-    local map = function(keys, func, desc, mode)
-      mode = mode or "n"
-      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-    end
+	callback = function(event)
+		local map = function(keys, func, desc, mode)
+			mode = mode or "n"
+			vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
 
-    map("K", vim.lsp.buf.hover, "Show Hover Information")
-    map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-    map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-    map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
-    map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
-    map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-    map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
-    map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
-    map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", "n")
-    map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+		map("K", vim.lsp.buf.hover, "Show Hover Information")
+		map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+		map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+		map("gi", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+		map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+		map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+		map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", "n")
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-    local client = vim.lsp.get_client_by_id(event.data.client_id)
-    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-      local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.document_highlight,
-      })
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+			local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.document_highlight,
+			})
 
-      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-        buffer = event.buf,
-        group = highlight_augroup,
-        callback = vim.lsp.buf.clear_references,
-      })
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = event.buf,
+				group = highlight_augroup,
+				callback = vim.lsp.buf.clear_references,
+			})
 
-      vim.api.nvim_create_autocmd("LspDetach", {
-        group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-        callback = function(event2)
-          vim.lsp.buf.clear_references()
-          vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-        end,
-      })
-    end
+			vim.api.nvim_create_autocmd("LspDetach", {
+				group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+				callback = function(event2)
+					vim.lsp.buf.clear_references()
+					vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
+				end,
+			})
+		end
 
-    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
-      map("<leader>ti", function()
-        local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })
-        vim.lsp.inlay_hint(
-          enabled and vim.lsp.inlay_hint.disable or vim.lsp.inlay_hint.enable,
-          { bufnr = event.buf }
-        )
-      end, "[T]oggle [I]nlay Hints")
-    end
-  end,
+		if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+			map("<leader>ti", function()
+				local enabled = vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf })
+				vim.lsp.inlay_hint(
+					enabled and vim.lsp.inlay_hint.disable or vim.lsp.inlay_hint.enable,
+					{ bufnr = event.buf }
+				)
+			end, "[T]oggle [I]nlay Hints")
+		end
+	end,
 })
 
 -- Global LSP Configuration
 lsp_zero.extend_lspconfig({
-  sign_text = true,
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
+	sign_text = true,
+	capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
 local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lsp_flags = {
-  allow_incremental_sync = true,
-  debounce_text_changes = 150,
+	allow_incremental_sync = true,
+	debounce_text_changes = 150,
 }
 
 -- LSP Servers
 lspconfig.clangd.setup({
-  capabilities = capabilities,
-  cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
-  init_options = {
-    fallback_flags = { "-std=c++17" },
-  },
-  flag = lsp_flags,
+	capabilities = capabilities,
+	cmd = { "clangd", "--background-index", "--clang-tidy", "--log=verbose" },
+	init_options = {
+		fallback_flags = { "-std=c++17" },
+	},
+	flag = lsp_flags,
 })
 
 local servers = {
-  "lua_ls",
-  "bashls",
-  "cssls",
-  "ts_ls",
-  "emmet_language_server",
-  "taplo",
-  "nil",
-  "html",
-  "css_variables",
+	"lua_ls",
+	"bashls",
+	"cssls",
+	"ts_ls",
+	"emmet_language_server",
+	"taplo",
+	"html",
+	"css_variables",
 }
 
 -- Loop through each server and configure
 for _, server in ipairs(servers) do
-  local config = {
-    capabilities = capabilities,
-    flags = lsp_flags,
-  }
-  lspconfig[server].setup(config)
+	local config = {
+		capabilities = capabilities,
+		flags = lsp_flags,
+	}
+	lspconfig[server].setup(config)
 end
--- nvim/.config/nvim/lua/config/null-ls.lua
+++ nvim/.config/nvim/lua/config/null-ls.lua
@@ -1,14 +1,15 @@
 local augoup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
+local fmt = null_ls.builtins.formatting
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.formatting.stylua,
-		null_ls.builtins.formatting.clang_format,
-		null_ls.builtins.formatting.shfmt,
-		null_ls.builtins.formatting.prettier,
-    --	null_ls.builtins.formatting.nixpkgs-fmt,
+		fmt.stylua,
+		fmt.clang_format,
+		fmt.shfmt,
+		fmt.prettier,
 	},
+
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augoup, buffer = bufnr })
