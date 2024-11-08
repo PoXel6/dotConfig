5762b15f195b5bdca74951bb13c24a646a4a459e
PoXel6 <iampoxel6@gmail.com>
2024-11-08 10:50:48 +0000

    made telescope keybinds to work with which-key

--- nvim/.config/nvim/lua/config/telescope/keymaps.lua
+++ nvim/.config/nvim/lua/config/telescope/keymaps.lua
@@ -1,41 +1,49 @@
 local M = {}
 
 M.setup = function()
+	local wk = require("which-key")
 	local builtin = require("telescope.builtin")
 
-	vim.keymap.set("n", "<leader>sg", builtin.git_files, { noremap = true, silent = true, desc = "[S]earch [G]it" })
-	vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
-	vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
-	vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
-	vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
-	vim.keymap.set(
-		"n",
-		"<leader>th",
-		":Telescope themes<CR>",
-		{ noremap = true, silent = true, desc = "[T]elescope [H] themes" }
-	)
+	wk.register({
+		["<leader>"] = {
+			sg = { builtin.git_files, "[S]earch [G]it" },
+			["<leader>"] = { builtin.buffers, "[ ] Find existing buffers" },
+			sd = { builtin.diagnostics, "[S]earch [D]iagnostics" },
+			sk = { builtin.keymaps, "[S]earch [K]eymaps" },
+			sf = { builtin.find_files, "[S]earch [F]iles" },
+			st = { ":Telescope themes<CR>", "[S]earch [T]hemes" },
+			sa = {
+				function()
+					builtin.find_files({ follow = true, no_ignore = true, hidden = true })
+				end,
+				"[S]earch [A]ll Files",
+			},
+			sn = {
+				function()
+					builtin.find_files({ cwd = vim.fn.stdpath("config") })
+				end,
+				"[S]earch [N]eovim files",
+			},
 
-	vim.keymap.set("n", "<leader>sa", function()
-		builtin.find_files({ follow = true, no_ignore = true, hidden = true })
-	end, { desc = "[S]earch [A]ll Files" })
+			so = {
+				function()
+					builtin.live_grep({ grep_open_files = true, prompt_title = "Live Grep in Open Files" })
+				end,
+				"[S]earch [O]pen Files",
+			},
+		},
+		-- Non-leader key bindings
+		["<C-p>"] = {
 
-	vim.keymap.set("n", "<leader>sn", function()
-		builtin.find_files({ cwd = vim.fn.stdpath("config") })
-	end, { desc = "[S]earch [N]eovim files" })
-
-	vim.keymap.set("n", "<C-p>", function()
-		builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
-			winblend = 10,
-			previewer = false,
-		}))
-	end, { desc = "[/] Fuzzily search in current buffer" })
-
-	vim.keymap.set("n", "<leader>so", function()
-		builtin.live_grep({
-			grep_open_files = true,
-			prompt_title = "Live Grep in Open Files",
-		})
-	end, { desc = "[S]earch [O]pen Files" })
+			function()
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end,
+			"[/] Fuzzily search in current buffer",
+		},
+	})
 end
 
 return M
