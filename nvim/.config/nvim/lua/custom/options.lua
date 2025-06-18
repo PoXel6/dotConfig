eb2b79a05439ff137701d90fc7b51b22d7d0002f
pox <iampoxel6@gmail.com>
2025-06-18 10:24:17 +0330

    update: vim options

--- nvim/.config/nvim/lua/custom/options.lua
+++ nvim/.config/nvim/lua/custom/options.lua
@@ -3,16 +3,20 @@ local o = vim.o
 local g = vim.g
 
 vim.loader.enable(true)
+
 g.mapleader = " "
 g.toggle_theme_icon = "   "
 g.netrw_banner = 0
+
 -- vim setting
 opt.relativenumber = true
 opt.termguicolors = true
+o.termguicolors = true
 opt.incsearch = true
 opt.nu = true
 opt.wrap = false
 
+-- Fold
 opt.foldmethod = "expr"
 opt.foldexpr = "nvim_treesitter#foldexpr()"
 opt.foldlevelstart = 99
@@ -22,15 +26,13 @@ o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 opt.showbreak = "↪ "
 opt.backspace = { "indent", "eol", "start" }
 
--- opt.colorcolumn = "100"
 opt.updatetime = 500
 opt.scrolloff = 10
 
-opt.clipboard = "unnamedplus"
--------------------------------------------
-
+-- Case
 o.ignorecase = true
 o.smartcase = true
+
 o.mouse = "a"
 
 -- Indenting
@@ -39,17 +41,3 @@ o.shiftwidth = 2
 o.smartindent = true
 o.tabstop = 2
 o.softtabstop = 2
-
--------------------------------------------
-
-o.termguicolors = true
-vim.o.guifont = "JetBrains Mono Nerd Font:h15"
--------------------------------------------
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
-	callback = function()
-		vim.highlight.on_yank()
-	end,
-})
