32dc889da87224bff2add06402dd9d516da8aa48
pox <iampoxel6@gmail.com>
2025-06-21 18:29:54 +0330

    remove: auto pair and auto tag (i'm becoming more chad by seconds

--- nvim/.config/nvim/lua/config/mini/mini.lua
+++ nvim/.config/nvim/lua/config/mini/mini.lua
@@ -2,7 +2,6 @@
 require("mini.diff").setup()
 require("mini.splitjoin").setup()
 require("mini.git").setup()
-require("mini.pairs").setup()
 require("mini.surround").setup()
 require("mini.ai").setup()
 require("mini.operators").setup()
@@ -10,7 +9,7 @@ require("mini.operators").setup()
 -- config modules
 require("mini.comment").setup(require("config.mini.comment"))
 require("mini.icons").setup(require("config.mini.icons"))
-require("mini.notify").setup(require('config.mini.notify'))
+require("mini.notify").setup(require("config.mini.notify"))
 
 --
 require("config.mini.hipatterns")
