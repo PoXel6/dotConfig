6ee90899e045f1a81e6815febfa8d35fa612e7df
pox <iampoxel6@gmail.com>
2025-06-25 09:47:13 +0330

    cleaned up none ls config

--- nvim/.config/nvim/lua/config/none-ls.lua
+++ nvim/.config/nvim/lua/config/none-ls.lua
@@ -1,21 +1,22 @@
 local augoup = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require("null-ls")
-local formatter = null_ls.builtins.formatting
-local linter = null_ls.builtins.diagnostics
+local fmt = null_ls.builtins.formatting
+local lint = null_ls.builtins.diagnostics
+local impl = null_ls.builtins.code_actions.impl
+local ref = null_ls.builtins.code_actions.refactoring
 
 null_ls.setup({
 	sources = {
-		null_ls.builtins.code_actions.impl,
-		null_ls.builtins.code_actions.refactoring,
+		impl,
+		ref,
 
-		formatter.stylua,
-		formatter.clang_format,
-		formatter.shfmt,
-		formatter.prettierd,
-		formatter.black,
-		formatter.isort,
+		fmt.stylua,
+		fmt.shfmt,
+		fmt.prettierd,
+		fmt.black,
+		fmt.isort,
 
-		linter.mypy,
+		lint.mypy,
 	},
 
 	on_attach = function(client, bufnr)
