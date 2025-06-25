5a91e29d2708a498423c05a438e5092b31829a76
pox <iampoxel6@gmail.com>
2025-06-25 06:44:16 +0330

    disable unused mini modules

--- nvim/.config/nvim/lua/config/mini/mini.lua
+++ nvim/.config/nvim/lua/config/mini/mini.lua
@@ -1,15 +1,10 @@
 -- Zero config modules
 require("mini.diff").setup()
-require("mini.splitjoin").setup()
-require("mini.git").setup()
 require("mini.surround").setup()
 require("mini.ai").setup()
 require("mini.operators").setup()
 
 -- config modules
-require("mini.comment").setup(require("config.mini.comment"))
 require("mini.icons").setup(require("config.mini.icons"))
 require("mini.notify").setup(require("config.mini.notify"))
-
---
 require("config.mini.hipatterns")
