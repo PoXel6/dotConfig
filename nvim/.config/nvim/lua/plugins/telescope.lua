994e4f9f8cac33d7cf36a11868be910ee79b38ff
pox <iampoxel6@gmail.com>
2025-06-25 06:48:46 +0330

    change telescope event to UIEnter and add mini icons as a deps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,14 @@
 return {
 	{
 		"nvim-telescope/telescope.nvim",
-		event = "VeryLazy",
+		event = "UIEnter",
 		tag = "0.1.8",
 		dependencies = {
 			"piersolenski/telescope-import.nvim",
 			"andrew-george/telescope-themes",
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-ui-select.nvim",
+			"echasnovski/mini.icons",
 			{
 				"nvim-telescope/telescope-fzf-native.nvim",
 				build = "make",
