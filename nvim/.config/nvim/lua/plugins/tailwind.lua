f0cc2233b44172d788555160efbb3326130c4d94
pox <iampoxel6@gmail.com>
2025-07-01 23:39:05 +0330

    inlined filetypes for tailwind

--- nvim/.config/nvim/lua/plugins/tailwind.lua
+++ nvim/.config/nvim/lua/plugins/tailwind.lua
@@ -1,26 +1,36 @@
 return {
-	"luckasRanarison/tailwind-tools.nvim",
-	name = "tailwind-tools",
-	build = ":UpdateRemotePlugins",
-	ft = require("custom.file_types.frontend"),
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter",
-		"nvim-telescope/telescope.nvim",
-		"neovim/nvim-lspconfig",
-	},
-	opts = {
-		---@type TailwindTools.Option
-		{
-			server = {
-				override = true, -- setup the server from the plugin if true
-				settings = {}, -- shortcut for `settings.tailwindCSS`
-				on_attach = function(client, bufnr) end, -- callback triggered when the server attaches to a buffer
-			},
-			conceal = {
-				enabled = true, -- can be toggled by commands
-				min_length = nil, -- only conceal classes exceeding the provided length
-				symbol = "󱏿", -- only a single character is allowed
-			},
-		},
-	},
+  "luckasRanarison/tailwind-tools.nvim",
+  name = "tailwind-tools",
+  build = ":UpdateRemotePlugins",
+  ft = {
+    "html",
+    "css",
+    "vue",
+    "js",
+    "ts",
+    "jsx",
+    "tsx",
+    "svelte",
+  },
+
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-telescope/telescope.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  opts = {
+    ---@type TailwindTools.Option
+    {
+      server = {
+        override = true,                         -- setup the server from the plugin if true
+        settings = {},                           -- shortcut for `settings.tailwindCSS`
+        on_attach = function(client, bufnr) end, -- callback triggered when the server attaches to a buffer
+      },
+      conceal = {
+        enabled = true, -- can be toggled by commands
+        min_length = nil, -- only conceal classes exceeding the provided length
+        symbol = "󱏿", -- only a single character is allowed
+      },
+    },
+  },
 }
