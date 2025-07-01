276a952d5950439327763e3ff0a150eb7ea365fa
pox <iampoxel6@gmail.com>
2025-07-01 23:39:13 +0330

    remove unused autocmds

--- nvim/.config/nvim/lua/custom/autocmd.lua
+++ nvim/.config/nvim/lua/custom/autocmd.lua
@@ -1,14 +1,6 @@
-vim.api.nvim_create_user_command("Config", function(opts)
-	vim.cmd("e ~/.config")
-end, { nargs = 0 })
-
-vim.api.nvim_create_user_command("Code", function(opts)
-	vim.cmd("e ~/CodeezNutz")
-end, { nargs = "?" })
-
 vim.api.nvim_create_autocmd("TextYankPost", {
-	desc = "Highlight when yanking (copying) text",
-	callback = function()
-		vim.highlight.on_yank()
-	end,
+  desc = "Highlight when yanking (copying) text",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
 })
