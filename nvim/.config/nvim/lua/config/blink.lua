144298cb1706acd2ee6d7c5fe25825177dc29f85
pox <iampoxel6@gmail.com>
2025-07-01 23:48:54 +0330

    remove luasnip snippets from blink completion

--- nvim/.config/nvim/lua/config/blink.lua
+++ nvim/.config/nvim/lua/config/blink.lua
@@ -1,64 +1,63 @@
 return {
-	keymap = {
-		preset = "default",
-		["<C-e>"] = {},
-	},
+  keymap = {
+    preset = "default",
+    ["<C-e>"] = {},
+  },
 
-	completion = {
-		documentation = {
-			auto_show = false,
-			auto_show_delay_ms = 0,
-			window = {
-				max_width = 35,
-				border = "rounded",
-			},
-		},
-		menu = {
-			draw = {
-				align_to = "label",
-				columns = {
-					{ "label", "label_description", gap = 3 },
-					{ "kind_icon" },
-					{ "kind" },
-				},
-				components = {
-					label = {
-						ellipsis = true,
-						width = {
-							fill = false,
-							min = 30,
-							max = 35,
-						},
-					},
-				},
-			},
-			max_height = 7,
-		},
-		ghost_text = { enabled = false },
-	},
+  completion = {
+    documentation = {
+      auto_show = false,
+      auto_show_delay_ms = 0,
+      window = {
+        max_width = 35,
+        border = "rounded",
+      },
+    },
+    menu = {
+      draw = {
+        align_to = "label",
+        columns = {
+          { "label",    "label_description", gap = 3 },
+          { "kind_icon" },
+          { "kind" },
+        },
+        components = {
+          label = {
+            ellipsis = true,
+            width = {
+              fill = false,
+              min = 30,
+              max = 35,
+            },
+          },
+        },
+      },
+      max_height = 7,
+    },
+    ghost_text = { enabled = false },
+  },
 
-	snippets = {
-		preset = "luasnip",
-	},
+  snippets = {
+    preset = "luasnip",
+  },
 
-	sources = {
-		default = {
-			"lazydev",
-			"lsp",
-			"path",
-			"snippets",
-			"buffer",
-		},
-		providers = {
-			lazydev = {
-				name = "LazyDev",
-				module = "lazydev.integrations.blink",
-				score_offset = 100,
-			},
-		},
-	},
+  sources = {
+    default = {
+      "lazydev",
+      "lsp",
+      "path",
+      "buffer",
+    },
+    providers = {
+      lazydev = {
+        name = "LazyDev",
+        module = "lazydev.integrations.blink",
+        score_offset = 100,
+      },
+    },
+  },
 
-	fuzzy = {
-		implementation = "prefer_rust_with_warning",
-	},
+  fuzzy = {
+    implementation = "prefer_rust_with_warning",
+  },
 }
