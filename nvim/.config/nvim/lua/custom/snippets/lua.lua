ed2c8e93c926a872b431c0fc85fe6cd7d14cee5c
pox <iampoxel6@gmail.com>
2025-07-02 14:20:54 +0330

    add snippet, snippet for snippets

--- nvim/.config/nvim/lua/custom/snippets/lua.lua
+++ nvim/.config/nvim/lua/custom/snippets/lua.lua
@@ -2,24 +2,46 @@ local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
+local rep = require('luasnip.extras').rep
+
+local lf_fmt = [[
+    local {} = function({})
+      {}
+    end
+    ]]
+
+local lf_inodes = {
+  i(1, "name"),
+  i(2, "params"),
+  i(0),
+}
 
 ls.add_snippets("lua", {
-  s(
-    "lf",
-    fmt(
-      [[
-    local <> = function(<>)
-      <>
-    end
-    ]],
-      {
-        i(1, "name"),
-        i(2, "params"),
-        i(0),
-      },
-      {
-        delimiters = "<>",
-      }
-    )
-  ),
+  s("lf", fmt(lf_fmt, lf_inodes, {})),
+})
+
+local snip_fmt = [[
+local {}_fmt = {}
+
+local {}_inodes = {{ {} }}
+
+ls.add_snippets("{}", {{
+  s("{}", fmt({}_fmt, {}_inodes, {{}})),
+}})
+{}]]
+
+local snip_inodes = {
+  i(1),
+  i(2, "fmt"),
+  rep(1),
+  i(3, "inodes"),
+  i(4, "lang"),
+  rep(1),
+  rep(1),
+  rep(1),
+  i(0),
+}
+
+ls.add_snippets("lua", {
+  s("snip", fmt(snip_fmt, snip_inodes, {})),
 })
