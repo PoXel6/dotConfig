d8b4b9c7178a0c7c60eb6ee8b2738fcb8e14d61b
pox <iampoxel6@gmail.com>
2025-07-05 12:00:38 +0330

    add keymap for formating the buffer in the normal anf visual mode

--- nvim/.config/nvim/lua/custom/keymaps/lsp.lua
+++ nvim/.config/nvim/lua/custom/keymaps/lsp.lua
@@ -1,5 +1,9 @@
 vim.keymap.set("n", "K", function()
-	vim.lsp.buf.hover({
-		border = "rounded",
-	})
+  vim.lsp.buf.hover({
+    border = "rounded",
+  })
 end, { desc = "Hover Informations", silent = true })
+
+vim.keymap.set({ "n", "v" }, "grf", function()
+  vim.lsp.buf.format()
+end, { desc = "Format the currect buffer", silent = true })
