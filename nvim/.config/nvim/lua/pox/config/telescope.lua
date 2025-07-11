ca3e24cfbbe5859913a94b6f8af1cdf6cc24c643
pox <iampoxel6@gmail.com>
2025-07-11 13:07:38 +0330

    add telescope ui-select back, it is actually usefull

--- nvim/.config/nvim/lua/pox/config/telescope.lua
+++ nvim/.config/nvim/lua/pox/config/telescope.lua
@@ -1,22 +1,24 @@
 local telescope = require("telescope")
 telescope.setup({
-	defaults = {
-		previewer = false,
-		prompt_prefix = "  >  ",
-		file_ignore_patterns = {
-			".gradle/",
-			"node_modules/",
-			".git/",
-			".idea/",
-			".vscode/",
-		},
-	},
-	fzf = {
-		fuzzy = true,
-		override_generic_sorter = true,
-		override_file_sorter = true,
-		case_mode = "smart_case",
-	},
+  defaults = {
+    previewer = false,
+    prompt_prefix = "  >  ",
+    file_ignore_patterns = {
+      ".gradle/",
+      "node_modules/",
+      ".git/",
+      ".idea/",
+      ".vscode/",
+    },
+  },
+  fzf = {
+    fuzzy = true,
+    override_generic_sorter = true,
+    override_file_sorter = true,
+    case_mode = "smart_case",
+  },
 
-	telescope.load_extension("fzf"),
+  telescope.load_extension("fzf"),
+  telescope.load_extension("ui-select")
+
 })
