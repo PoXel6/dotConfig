b1f97677374697d9d7dd8dc047a282b4f4bc9a6e
pox <iampoxel6@gmail.com>
2025-07-23 07:05:58 +0330

    remove: format on save from none-ls

--- nvim/.config/nvim/lua/pox/config/none-ls.lua
+++ nvim/.config/nvim/lua/pox/config/none-ls.lua
@@ -6,28 +6,15 @@ local impl = null_ls.builtins.code_actions.impl
 local ref = null_ls.builtins.code_actions.refactoring
 
 null_ls.setup({
-	sources = {
-		impl,
-		ref,
+  sources = {
+    impl,
+    ref,
 
-		fmt.stylua,
-		fmt.shfmt,
-		fmt.black,
-		fmt.isort,
+    fmt.stylua,
+    fmt.shfmt,
+    fmt.black,
+    fmt.isort,
 
-		lint.mypy,
-	},
-
-	on_attach = function(client, bufnr)
-		if client:supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augoup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augoup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = bufnr })
-				end,
-			})
-		end
-	end,
+    lint.mypy,
+  },
 })
