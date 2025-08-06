31c7684042822fa2f1b0fbca7b98e0f19273c817
pox <iampoxel6@gmail.com>
2025-08-06 09:46:45 +0330

    fix: move last two lsp remap to the init file and deleted the lsp.lua keymap file

--- nvim/.config/nvim/lua/pox/custom/keymaps/lsp.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set({ "n", "v" }, "gro", function()
-  vim.lsp.buf.format()
-end, { desc = "Format the currect buffer", silent = true })
-
-vim.keymap.set("n", "K", function()
-    vim.lsp.buf.hover({ border = "rounded", max_width = 60, max_height = 10 })
-end, { desc = "Hover information", silent = true })
