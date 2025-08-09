b83293727b5295bdf795a7b22eeabc28cd99db66
pox <iampoxel6@gmail.com>
2025-08-09 09:29:13 +0330

    add: configuration for rust_analyzer

--- nvim/.config/nvim/lua/pox/config/lsp/servers.lua
+++ nvim/.config/nvim/lua/pox/config/lsp/servers.lua
@@ -1,129 +1,156 @@
 return {
 
-  zls = {},
+    zls = {},
 
-  jdtls = {},
+    jdtls = {},
 
-  jsonls = {},
+    jsonls = {},
 
-  biome = {
-    single_file_support = true,
-    filetypes = {
-      "astro",
-      "css",
-      "graphql",
-      "html",
-      "javascript",
-      "javascriptreact",
-      "json",
-      "jsonc",
-      "svelte",
-      "typescript",
-      "typescript.tsx",
-      "typescriptreact",
-      "vue",
-    },
-  },
-
-  vtsls = {
-    settings = {
-      vtsls = {
-        tsserver = {
-          globalPlugins = {
-            {
-              name = "@vue/typescript-plugin",
-              location = vim.fn.stdpath('data') ..
-                  "/mason/packages/vue-language-server/node_modules/@vue/language-server",
-              languages = { "typescript", "javascript", "vue" },
-              enableForWorkspaceTypeScriptVersions = true,
-              configNamespace = "typescript",
-            },
-          },
+    biome = {
+        single_file_support = true,
+        filetypes = {
+            "astro",
+            "css",
+            "graphql",
+            "html",
+            "javascript",
+            "javascriptreact",
+            "json",
+            "jsonc",
+            "svelte",
+            "typescript",
+            "typescript.tsx",
+            "typescriptreact",
+            "vue",
         },
-      },
     },
-    filetypes = {
-      "javascript",
-      "javascriptreact",
-      "javascript.jsx",
-      "typescript",
-      "typescriptreact",
-      "typescript.tsx",
-      "vue",
-    },
-  },
 
-  cssls = {},
-
-  emmet_language_server = {
-    filetypes = {
-      "css",
-      "eruby",
-      "html",
-      "javascriptreact",
-      "typescriptreact",
-      "vue",
-      "less",
-      "sass",
-      "scss",
-      "pug",
+    vtsls = {
+        settings = {
+            vtsls = {
+                tsserver = {
+                    globalPlugins = {
+                        {
+                            name = "@vue/typescript-plugin",
+                            location = vim.fn.stdpath('data') ..
+                                "/mason/packages/vue-language-server/node_modules/@vue/language-server",
+                            languages = { "typescript", "javascript", "vue" },
+                            enableForWorkspaceTypeScriptVersions = true,
+                            configNamespace = "typescript",
+                        },
+                    },
+                },
+            },
+        },
+        filetypes = {
+            "javascript",
+            "javascriptreact",
+            "javascript.jsx",
+            "typescript",
+            "typescriptreact",
+            "typescript.tsx",
+            "vue",
+        },
     },
-  },
 
-  volar = {
-    init_options = {
-      vue = {
-        hybridMode = true,
-      },
-    },
-  },
+    cssls = {},
 
-  clangd = {
-    cmd = {
-      "clangd",
-      "--background-index",
-      "--clang-tidy",
+    emmet_language_server = {
+        filetypes = {
+            "css",
+            "eruby",
+            "html",
+            "javascriptreact",
+            "typescriptreact",
+            "vue",
+            "less",
+            "sass",
+            "scss",
+            "pug",
+        },
     },
-    init_options = {
-      fallback_flags = { "-std=c++17" },
-    },
-  },
 
-  lua_ls = {
-    codelens = {
-      enable = true,
+    volar = {
+        init_options = {
+            vue = {
+                hybridMode = true,
+            },
+        },
     },
-    hints = {
-      enable = true,
-      setType = false,
-      paramType = true,
-      paramName = "Disable",
-      semicolon = "Disable",
-      arrayIndex = "Disable",
+
+    clangd = {
+        cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+        },
+        init_options = {
+            fallback_flags = { "-std=c++17" },
+        },
     },
-  },
 
-  gopls = {
-    completeUnimported = true,
-    usePlaceholders = true,
-    analyses = {
-      unusedparams = true,
+    lua_ls = {
+        codelens = {
+            enable = true,
+        },
+        hints = {
+            enable = true,
+            setType = false,
+            paramType = true,
+            paramName = "Disable",
+            semicolon = "Disable",
+            arrayIndex = "Disable",
+        },
     },
-    hints = {
-      assignVariableTypes = true,
-      compositeLiteralFields = true,
-      compositeLiteralTypes = true,
-      constantValues = true,
-      functionTypeParameters = true,
-      parameterNames = true,
-      rangeVariableTypes = true,
+
+    gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+            unusedparams = true,
+        },
+        hints = {
+            assignVariableTypes = true,
+            compositeLiteralFields = true,
+            compositeLiteralTypes = true,
+            constantValues = true,
+            functionTypeParameters = true,
+            parameterNames = true,
+            rangeVariableTypes = true,
+        },
     },
-  },
 
-  pyright = {},
+    pyright = {},
 
-  ruff = {},
+    ruff = {},
 
-  rust_analyzer = {},
+    rust_analyzer = {
+        settings = {
+            ["rust-analyzer"] = {
+                imports = {
+                    granularity = {
+                        group = "module",
+                    },
+                    prefix = "self",
+                },
+
+                cargo = {
+                    buildScripts = {
+                        enable = true,
+                    },
+                },
+
+                procMacro = {
+                    enable = true
+                },
+
+                diagnostics = {
+                    styleLints = {
+                        enable = true
+                    }
+                }
+            }
+        }
+    },
 
 }
